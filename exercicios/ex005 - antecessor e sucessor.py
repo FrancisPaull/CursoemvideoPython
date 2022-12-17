963899d55fd396cd614a8db512dfaefa363f90a7
Francis Paul <96993153+FrancisPaull@users.noreply.github.com>
2022-12-17 09:50:57 -0300

    exerc-17-12-22

---
+++ exercicios/ex005 - antecessor e sucessor.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um número inteiro e mostre na tela seu antecessor e sucessor.
+
+n1 = int(input('Digite um número:'))
+ant = n1 - 1
+suc = n1 + 1
+print('O número {}, têm {} como antecessor e {} como sucessor.'.format(n1,ant,suc))
---
+++ exercicios/ex006 dobro triplo raiz quadrada.py
@@ -0,0 +1,5 @@
+#crie um algoritmo que leia um número e mostre seu dobro, triplo e raiz quadrada.
+
+n1 = int(input('Digite um número: '))
+dobro = n1 * 2
+print('O dobro de {} é igual a {}'.format(n1, dobro))
