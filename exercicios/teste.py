90e5f12aff2aa7c8682d8d1fba57190c4593be32
Francis Paul <96993153+FrancisPaull@users.noreply.github.com>
2023-01-18 16:07:08 -0300

    ..

--- exercicios/teste.py
+++ exercicios/teste.py
@@ -1,9 +1,22 @@
-frase = 'Ler ao contrário'
-lista = [frase]
-print(frase)
-inicio = len(frase) - 1
-while inicio != -1:
-    for letra in lista:
-        print(letra[inicio],end='')
-        inicio -= 1
+def not_string(frase):
+  if frase[0:2] == 'not':
+     return True
+  else:
+      frase = "not " + frase
+      return frase
 
+
+
+print(not_string('not quql'))
+
+print(not_string('x'))
+
+print(not_string('not é ruim'))
+
+print(not_string('ruim'))
+
+print(not_string('not'))
+
+print(not_string('not é'))
+
+print(not_string('not'))
